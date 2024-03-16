13704ae57cbb083d38666d230a31aa9a3832fb4d
Bo Tran <55688614+brass-pixel@users.noreply.github.com>
2024-03-16 16:46:57 +1100

    Issue with order.getMeals() returning Null

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -1,23 +1,33 @@
 package main;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 public class Menu {
 
 	public static void main(String[] args) {
 		// Console Menu Start and instantiate menu_input to create While Loop
 		char menu_input = 'e';
+		ArrayList<Order> orders = new ArrayList<Order>();
+		Burrito burrito = new Burrito();
+		Fries fries = new Fries();
+		Soda soda = new Soda();
+		Meal meal = new Meal();
 
 		printTitle();
 		while (menu_input != 'd') {
 			switch (menuOptions()) {
 			// Order
 			case 'a':
-				// TODO Create Order Method + store data from Orders?
+				Order new_order = orderFood(burrito, fries, soda, meal);
+				orders.add(new_order);
+				
 				break;
 			// Show Sales Report
 			case 'b':
 				// TODO Create Show Sales Report Method
+				System.exit(0);
 				break;
 			// Update prices
 			case 'c':
@@ -42,11 +52,7 @@ public class Menu {
 	public static char menuOptions() {
 		// Create Scanner Object and show menu Options
 		Scanner menu_input = new Scanner(System.in);
-		System.out.printf("%3s\n", "a) Order");
-		System.out.printf("%3s\n", "b) Show Sales report");
-		System.out.printf("%3s\n", "c) Update prices");
-		System.out.printf("%3s\n", "d) Exit");
-		System.out.print("Please select: ");
+		printMenuOptions();
 
 		// Infinite loop to convert String to char upon valid input + error handling
 		do {
@@ -70,6 +76,101 @@ public class Menu {
 		} while (true);
 
 	}
-	
-	
+
+	public static Order orderFood(Burrito burrito, Fries fries, Soda soda, Meal meal) {
+		Order order = new Order();
+		int user_choice = -1;
+
+		// Move this variable somewhere else
+		int cook_fries_counter = 0;
+
+		Scanner menu_input = new Scanner(System.in);
+
+		printOrderOptions();
+		do {
+			// Take user input and error handling for non-int inputs
+			try {
+				user_choice = menu_input.nextInt();
+
+				switch (user_choice) {
+				case 1:
+					order.addItem(burrito);
+					break;
+				case 2:
+					// TODO Fries code for warming tray
+					order.addItem(fries);
+					cook_fries_counter++;
+					break;
+				case 3:
+					order.addItem(soda);
+					break;
+				case 4:
+					// TODO add meal setter method
+					order.addMeal(meal);
+					break;
+				case 5:
+					break;
+				default:
+					System.out.println("Please provide an option between 1-5");
+				}
+			} catch (InputMismatchException e) {
+				// Handle the case where the input is not an integer
+				System.out.println("Please enter a valid integer between 1-5");
+				menu_input.next(); // Clear the invalid input from the scanner
+			}
+
+		} while (user_choice != 5);
+		
+		// Finalise Order Details
+		order.setTotalPrice();
+		order.setTotalPrepTime(cook_fries_counter);
+		order.printOrderTotal();
+		int money = 0;
+		
+		// User enters in amount to pay for order
+		do {
+			try {
+				System.out.print("Please enter money: ");
+				money = menu_input.nextInt();
+				
+				// Resolve Order if payable
+				if (money >= order.getTotalPrice()) {
+
+					System.out.printf("Change returned $%d\n", (money - order.getTotalPrice()));
+					System.out.printf("Time taken: %d minutes\n\n", order.getTotalPrepTime());
+					
+				}
+				// Tell user to provide money enough to pay for order
+				else {
+					System.out.println("Sorry, that's not enough to pay for order\n");
+				}
+				
+			} catch (InputMismatchException e) {
+				// Handle the case where the input is not an integer
+				System.out.println("Please enter a valid integer amount");
+				menu_input.next(); // Clear the invalid input from the scanner
+			}
+		} while (money < order.getTotalPrice());
+
+		return order;
+	}
+
+	public static void printMenuOptions() {
+		System.out.printf("%3s\n", "a) Order");
+		System.out.printf("%3s\n", "b) Show Sales report");
+		System.out.printf("%3s\n", "c) Update prices");
+		System.out.printf("%3s\n", "d) Exit");
+		System.out.print("Please select: ");
+	}
+
+	public static void printOrderOptions() {
+		System.out.println("> Select the food item");
+		System.out.printf("%3s\n", "1. Burrito");
+		System.out.printf("%3s\n", "2. Fries");
+		System.out.printf("%3s\n", "3. Soda");
+		System.out.printf("%3s\n", "4. Meal");
+		System.out.printf("%3s\n", "5. No More");
+		System.out.print("Please select: ");
+	}
+
 }
--- src/main/Order.java
+++ src/main/Order.java
@@ -1,12 +1,13 @@
 package main;
+
 import java.util.ArrayList;
 
-public class Order extends Meal{
+public class Order extends Meal {
 	private ArrayList<FoodItem> items;
 	private ArrayList<Meal> meals;
 	private int total_prep_time;
 	private float total_price;
-	
+
 	// CONSTRUCTOR
 	public Order() {
 		this.items = new ArrayList<FoodItem>();
@@ -17,81 +18,107 @@ public class Order extends Meal{
 
 	// GETTERS
 	public ArrayList<FoodItem> getOrderItems() {
-        return this.items;
-    } 
-	
+		return this.items;
+	}
+
 	public ArrayList<Meal> getMeals() {
-        return this.meals;
-    } 
-	
+		return this.meals;
+	}
+
 	public int getTotalPrepTime() {
-        return this.total_prep_time;
-    }
-	
+		return this.total_prep_time;
+	}
+
 	public float getTotalPrice() {
-        return this.total_price;
-    }
+		return this.total_price;
+	}
+
 	// SETTERS
-	public void addItems(FoodItem item) {
+	public void addItem(FoodItem item) {
 		this.items.add(item);
-	} 
-	
+	}
+
+	public void addMeal(Meal meal) {
+		this.meals.add(meal);
+	}
+
 	// TODO Optimise Code
 	public void setTotalPrepTime(int cook_fries_counter) {
 		int burrito_quantity = countItemQuantity("Burrito");
-		
+
 		// Checks which counter is higher to get the correct total prep time
 		if (burrito_quantity > cook_fries_counter) {
-			for (FoodItem item:this.getItems()) {
+			for (FoodItem item : this.getItems()) {
 				if (item instanceof Burrito) {
 					// Burrito Prep Time = Math.ceil(b/2) * 9 min
-					this.total_prep_time = (int) Math.ceil(burrito_quantity/item.getMaxSimultaneousPrep()) * item.getPrepTime();
+					this.total_prep_time = (int) Math.ceil(burrito_quantity / item.getMaxSimultaneousPrep())
+							* item.getPrepTime();
 					break;
 				}
 			}
 		} else {
-			for (FoodItem item:this.getItems()) {
+			for (FoodItem item : this.getItems()) {
 				if (item instanceof Fries) {
 					// Fries Prep Time = cook_fries_counter * 8 min
 					this.total_prep_time = (int) cook_fries_counter * item.getPrepTime();
 					break;
 				}
 			}
-		};
-		
+		}
+		;
+
 	}
-	
+
 	public void setTotalPrice() {
 		float total_price = 0;
-		
+
 		// FoodItem Loop
-		for(FoodItem item:this.getItems()) {
+		for (FoodItem item : this.getItems()) {
 			total_price += item.getPrice();
-		};
-		
+		}
+		;
+
 		// Meals Loop
-		for(Meal meal:this.getMeals()) {
+		for (Meal meal : this.getMeals()) {
 			total_price += meal.getTotalPrice();
 		}
-		
+	
+
 		this.total_price = total_price;
 	}
-	
+
 	// ADDITIONAL METHODS
 	public int countItemQuantity(String item_name) {
 		int quantity = 0;
-		
+
 		// FoodItem Loop to find specific FoodItem
-		for (FoodItem item:this.getItems()) {
+		for (FoodItem item : this.getItems()) {
 			if (item.getName().equals(item_name)) {
 				quantity++;
 			}
-		};
-		
+		}
+		;
+
 		// Add quantity of meals to item quantity
 		quantity += this.getMeals().size();
-		
+
 		return quantity;
+	}
+
+	public void printOrderTotal() {
+		int[] items_quantity = { countItemQuantity("Burrito"), countItemQuantity("Fries"), countItemQuantity("Soda") };
+		String[] item_names = { "Burrito", "Fries", "Soda" };
+
+		System.out.print("Total for ");
+
+		// Loop through to print each food item in Order if it exists.
+		for (int i = 0; i < 3; i++) {
+			if (items_quantity[i] > 0) {
+				System.out.print(items_quantity[i] + item_names[i] + ", ");
+			}
+		}
+
+		System.out.println("is " + this.getTotalPrice());
 	}
 
 }
